d5aaa76eb6faddf5f15f1f2438b6d4e51a6964ab
evd3v@yandex.ru <evd3v@yandex.ru>
2019-08-28 22:46:58 +0300

    hotfix

--- js/common.js
+++ js/common.js
@@ -44,4 +44,11 @@ window.onload = function() {
         }, 200);
     };
 
+
+    // Example 1: From an element in DOM
+    $('.open-popup-link').magnificPopup({
+        type:'inline',
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+     });
+  
 };
