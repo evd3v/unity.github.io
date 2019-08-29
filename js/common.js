42088abcbbe865f8002c1a830bf0bf8070482ef4
evd3v@yandex.ru <evd3v@yandex.ru>
2019-08-29 23:03:56 +0300

    hotfix

--- js/common.js
+++ js/common.js
@@ -76,13 +76,14 @@ window.onload = function() {
 
     /* video popup */
 
-    $('.play-video__link').magnificPopup({
-        type:'iframe',
-        disableOn: 700,
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false,
-        });
-  
+    $(".play-video").magnificPopup({
+        delegate: 'a',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+          beforeOpen: function() {
+             this.st.mainClass = this.st.el.attr('data-effect');
+          }
+        },
+        midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
 };
