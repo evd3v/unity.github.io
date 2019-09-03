f1d3050b77067863db24e83c07b402485e6e728c
evd3v@yandex.ru <evd3v@yandex.ru>
2019-09-03 19:13:37 +0300

    refactoring slider, some fixes

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
 
     /* resize the header text on resize window */
 
@@ -6,8 +6,8 @@ window.onload = function() {
     let headerText = document.querySelector(".header__text");
     let sizeOfHeader = 100;
 
-    let checkHeaderSize = function() {
-        if(header.offsetWidth > document.documentElement.clientWidth / 1.5) {
+    let checkHeaderSize = function () {
+        if (header.offsetWidth > document.documentElement.clientWidth / 1.5) {
             headerText.style.fontSize = sizeOfHeader-- + "px";
             checkHeaderSize();
         } else {
@@ -24,41 +24,41 @@ window.onload = function() {
     let playVideo = document.querySelector(".play-video__link");
     let playVideoText = ['p', 'l', 'a', 'y', ' ', 'v', 'i', 'd', 'e', 'o'];
 
-    playVideo.onmouseenter = function() {
+    playVideo.onmouseenter = function () {
         let element = document.querySelector(".play-video__text");
         element.innerHTML = '';
         element.classList.remove('play-video__text--in');
         element.classList.remove('play-video__text--out');
-        element.style.display = "block";
+        element.style.visibility = "visible";
         setTimeout(() => {
             element.classList.add('play-video__text--in');
-            playVideoText.forEach(function(char, index) {
+            playVideoText.forEach(function (char, index) {
                 setTimeout(() => {
                     element.innerHTML += char;
-                }, index*25);
+                }, index * 25);
             });
         }, 200);
     }
 
-    playVideo.onmouseleave = function() {
+    playVideo.onmouseleave = function () {
         let element = document.querySelector(".play-video__text");
         setTimeout(() => {
             element.classList.add('play-video__text--out');
         }, 200);
         setTimeout(() => {
-            element.style.display = "none";
+            element.style.visibility = "hidden";
         }, 400);
     };
 
     /* preventDefault links */
 
-    $("a").click(function(e) {
+    $("a").click(function (e) {
         e.preventDefault();
     });
 
     /* overlay-menu */
 
-    $(".header-menu__link").click(function(e) {
+    $(".header-menu__link").click(function (e) {
         $(".header-menu-overlay__list").css({
             'opacity': '1',
         });
@@ -68,7 +68,7 @@ window.onload = function() {
         });
     });
 
-    $(".header-menu-overlay__close").click(function(e) {
+    $(".header-menu-overlay__close").click(function (e) {
         $(".header-menu-overlay").css({
             'width': '0%',
             'visibility': 'hidden',
@@ -81,7 +81,7 @@ window.onload = function() {
 
     /* bg-parallax effect on scroll */
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         let scrollPosition = $(this).scrollTop();
         $(".page__firstpage-bg").css({
             "transform": "translate(0, " + scrollPosition / 20 + "%)",
@@ -94,16 +94,16 @@ window.onload = function() {
         document.body.clientHeight, document.documentElement.clientHeight
     );
 
-    $(".play-video__link")[0].onclick = function() {
+    $(".play-video__link")[0].onclick = function () {
         $(".play-video__content").css({
             'visibility': 'visible',
             'opacity': '1',
             'transform': 'scale(1)',
         });
         $('.page__firstpage-overlay').css({
-            'visibility' : 'visible', 
-            'height' : scrollHeight + 'px',
-            'width' : '100%',
+            'visibility': 'visible',
+            'height': scrollHeight + 'px',
+            'width': '100%',
             'opacity': '0.75',
         });
         $(".play-video__close").css({
@@ -111,15 +111,15 @@ window.onload = function() {
         })
     }
 
-    $(".play-video__close")[0].onclick = function() {
+    $(".play-video__close")[0].onclick = function () {
         $(".play-video__content").css({
             'visibility': 'hidden',
             'opacity': '0',
             'transform': 'scale(0)',
         });
         $('.page__firstpage-overlay').css({
-            'visibility' : 'hidden', 
-            'opacity' : '0',
+            'visibility': 'hidden',
+            'opacity': '0',
         });
         $(".play-video__close").css({
             'transform': 'rotate(0deg)',
@@ -127,139 +127,92 @@ window.onload = function() {
     }
 
     /* servise slider */
-	let body = document.querySelector('body');
-	let serviseSlider = document.querySelector(".service-slider__wrapper");
-    body.ondragstart = function() { return false;}; /* disable drag&drop */
-    const itemWidth = $(".service-slider-item")[0].getBoundingClientRect().width; /* to determine width of the item */
-    let startPosition = 0; /* начальная координата от края блока */
+    let serviceSliderOn = function () {
+        $('body').ondragstart = function (event) {
+            event.preventDefault(); /* запрещаем тянучку блоков */
+        };
 
-	serviseSlider.onmousedown = function(event) {
-        serviseSlider.style.cursor = "move";
-        let currentOffset = Math.abs(startPosition) + event.offsetX; // расстояние от левого края блока wrapper до точки клика
-        let startX = event.offsetX; // координата точки клика относительно текущего блока
-        let startOffsetX = event.clientX; // координата точки клика относительно окна браузера
-        console.log(event.clientX);
-		serviseSlider.onmousemove = function(e) {
-            let offsetX = startOffsetX - e.clientX; // расстояние, на которое сдвинулась мышь от места клика
-			if( 0 < currentOffset && currentOffset < itemWidth && offsetX < 0 ) {
-				serviseSlider.style.left = startPosition - offsetX / 5 + "px";
-			} else
-			if( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX > 0 )
-			{
-				serviseSlider.style.left = startPosition - offsetX / 5 + "px";
-            } else
-            if( offsetX > 0 ){
-                serviseSlider.style.left = startPosition - offsetX + "px"; // двигаем блок влево 
-            } else 
-            if ( offsetX < 0) {
-                serviseSlider.style.left = startPosition + Math.abs(offsetX) + "px"; // двигаем блок вправо 
-            }
-
-            document.onmouseup = function(e) {
-                if( 0 < currentOffset && currentOffset < itemWidth && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition - itemWidth + 'px';
-                    startPosition -= itemWidth;
-                } else 
-                if ( 0 < currentOffset && currentOffset < itemWidth && offsetX < 0 ) {
-                    serviseSlider.style.left = startPosition + 'px';
-                } else 
-                if ( itemWidth < currentOffset && currentOffset < itemWidth * 2 && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition - itemWidth + 'px';
-                    startPosition -= itemWidth;
-                } else 
-                if ( itemWidth < currentOffset && currentOffset < itemWidth * 2 && offsetX < 0 ) {
-                    serviseSlider.style.left = startPosition + itemWidth + 'px';
-                    startPosition += itemWidth;
-                } else 
-                if ( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition + 'px';
-                } else
-                if ( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX < 0 ) {
-                    serviseSlider.style.left = startPosition + itemWidth + 'px';
-                    startPosition += itemWidth;
-                }
-
-                serviseSlider.onmousemove = null;
-                serviseSlider.onmouseup = null;
-                serviseSlider.style.cursor = "auto";
-            }
-
-		}	
-
-    }
-    
-    serviseSlider.ontouchstart = function(event) {
-        let currentOffset = Math.abs(startPosition) + event.touches[0].clientX; // расстояние от левого края блока wrapper до точки клика
-        let startX = event.offsetX; // координата точки клика относительно текущего блока
-        let startOffsetX = event.touches[0].clientX; // координата точки клика относительно окна браузера
-		serviseSlider.ontouchmove = function(e) {
-            let offsetX = startOffsetX - e.touches[0].clientX; // расстояние, на которое сдвинулась мышь от места клика
-			if( 0 < currentOffset && currentOffset < itemWidth && offsetX < 0 ) {
-				serviseSlider.style.left = startPosition - offsetX / 10 + "px";
-			} else
-			if( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX > 0)
-			{
-				serviseSlider.style.left = startPosition - offsetX / 10 + "px";
-            } else
-            if( offsetX > 0 ){
-                serviseSlider.style.left = startPosition - offsetX + "px"; // двигаем блок влево 
-            } else 
-            if ( offsetX < 0) {
-                serviseSlider.style.left = startPosition + Math.abs(offsetX) + "px"; // двигаем блок вправо 
-            }
-            serviseSlider.ontouchend = function() {
-                if( 0 < currentOffset && currentOffset < itemWidth && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition - itemWidth + 'px';
-                    startPosition -= itemWidth;
-                } else 
-                if ( 0 < currentOffset && currentOffset < itemWidth && offsetX < 0 ) {
-                    serviseSlider.style.left = startPosition + 'px';
-                } else 
-                if ( itemWidth < currentOffset && currentOffset < itemWidth * 2 && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition - itemWidth + 'px';
-                    startPosition -= itemWidth;
-                } else 
-                if ( itemWidth < currentOffset && currentOffset < itemWidth * 2 && offsetX < 0 ) {
-                    serviseSlider.style.left = startPosition + itemWidth + 'px';
-                    startPosition += itemWidth;
-                } else 
-                if ( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition + 'px';
-                } else
-                if ( itemWidth * 2 < currentOffset && currentOffset < itemWidth * 3 && offsetX < 0) {
-                    serviseSlider.style.left = startPosition + itemWidth + 'px';
-                    startPosition += itemWidth;
-                } else 
-                if  ( currentOffset > itemWidth * 3 && offsetX > 0 ) {
-                    serviseSlider.style.left = startPosition + 'px';
-                }
-                serviseSlider.ontouchmove = null;
-                serviseSlider.ontouchend = null;
-            }
-		}
-    }
-
-    /* slider-paginator */
-    $.each($(".service-slider__paginator-link"), function(element) {
-        element.onclick = function(event) {
-            event.preventDefault();
-        }
-    });
-
-    let paginators = $('.service-slider__paginator')[0].children;
-    for(let i = 0; i < paginators.length; i++) {
-        let numberOfElements;
-        paginators[i].onclick = function() {
-            numberOfElements = i;
-            serviseSlider.style.left = -(numberOfElements * itemWidth) + 'px';
-            for(let j = 0; j < paginators.length; j++) {
-                console.dir(paginators[j].children[0].classList);
-                paginators[j].children[0].classList.remove('paginator-item--selected');
-            }
-            paginators[i].children[0].classList.add('paginator-item--selected');
-        }
-    }
+        let slider = $('.service-slider__wrapper'); /* блок слайдера, который будем двигать */
+        let slides = $('.service-slider__wrapper .service-slider-item'); /* массив слайдов */
+        let elementsCount = $(slides).length; /* определяем количество слайдов */
+        let paginators = $('.service-slider__paginator li') /* массив пагинаторов */
+        let slideOffset = 0;
+        const itemWidth = $('.service-slider-item')[0].getBoundingClientRect().width; /* определяем ширину блока */
 
 
+        slider.css({
+            'left': '0px', /* задаем начальный стиль для слайдера (необходимо для анимации) */
+        });
+
+        $.each(slides, function (i, element) {
+            $(element).css({
+                'left': slideOffset + '%'
+            });
+            slideOffset += 100;
+
+            $(element).bind('mousedown touchstart', function (event) {
+                slider.css({
+                    'cursor': 'move',
+                });
+
+                let startOffsetX = event.clientX || event.touches[0].clientX; /* координата точки клика относительно окна браузера */
+                let currentElement = i; /* определяем на какой элемент кликнули */
+                $(element).bind('mousemove touchmove', function (event) {
+                    let movedOffsetX = event.clientX || event.touches[0].clientX;
+                    let currentOffsetX = movedOffsetX - startOffsetX /* расстояние, на которое сдвинулась мышь после клика */
+                    // console.log(currentOffsetX);
+                    let nextElement = currentElement; /* следующий слайд */
+
+                    currentOffsetX > 0 ? nextElement-- : nextElement++; /* определяем номер следующего слайда */
+
+                    if ((currentElement == 0 && currentOffsetX > 0) ||
+                        (currentElement == elementsCount - 1 && currentOffsetX < 0)) {
+                            currentOffsetX /= 5;
+                            nextElement = i /* если двигаем слайдер за границу - следующий слайд = текущий слайд */
+                        }
+                    
+                    console.log(-Math.abs(currentElement * itemWidth) + currentOffsetX);
+                    slider.css({
+                        'left': -Math.abs(currentElement * itemWidth) + currentOffsetX + 'px',
+                    });
+                    
+                    $(document).bind('mouseup touchend', function() {
+
+                        slider.css({
+                            'left': -Math.abs(nextElement * itemWidth) + 'px',
+                            'cursor': 'auto',
+                        });
+
+                        /* changePaginator */
+
+                        $.each(paginators, function(i, element) {
+                            $(element).removeClass('paginator-item--selected')
+                        })
+                        $(paginators[nextElement]).addClass('paginator-item--selected');
+
+                        $(element).unbind('mousemove touchmove');
+                        $(document).unbind('mouseup touchend');
+
+                    });
+                });
+            });
+        });
+
+        /* slider paginator */
+        $.each(paginators, function(i, element) {
+            $(element).bind('click touch', function() {
+                $.each(paginators, function(i, element) {
+                    $(element).removeClass('paginator-item--selected')
+                })
+                $(paginators[i]).addClass('paginator-item--selected');
+
+                slider.css({
+                    'left': -Math.abs(i * itemWidth) + 'px',
+                });
+            });
+        });
+
+    };
+    serviceSliderOn();
 
 };
